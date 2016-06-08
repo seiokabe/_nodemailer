b6fbe1c6087933893b7de0c479c5584a24bf039e
Se.Okabe <okabe@nexia.jp>
2016-06-08 18:45:51 +0900

    update ben

--- nodemailer.js
+++ nodemailer.js
@@ -15,12 +15,62 @@
 //   }
 // }
 //
-
-var Conf  = require("./Conf.json");
+var Conf;
 var error = console.error
 var log   = console.log;
 var nodemailer = require("nodemailer");
 var smtpTransport = require('nodemailer-smtp-transport');
+var fs    = require('fs');
+var path  = require("path")
+var argv  = require('argv');
+
+var scriptname = ( process.argv[ 1 ] || '' ).split( '/' ).pop();
+
+argv.option([
+  {
+    name: 'Conf',
+    short: 'C',
+    type : 'string',
+    description :'Use alternate configuration file.',
+    example: "'"+scriptname+" -C xxxx.json'"
+  }
+]);
+
+var args = argv.run();
+var opt  = args.options;
+
+if ( opt["help"] ){
+  argv.help();
+  process.exit(0);
+}
+
+if (typeof opt["Conf"] === 'undefined') Conf = require("./Conf.json");
+else {
+  var file = opt["Conf"];
+  try {
+
+    var stats = fs.statSync(file);
+    Conf = require(file);
+    log("load Confing file: ", file)
+
+  } catch(e) {
+
+    try {
+      file = './' + file;
+      var stats = fs.statSync(file);
+      Conf = require(file);
+      log("load Confing file: ", file)
+    } catch(e) {
+      return error("Error: Conf is not found.");
+    }
+
+  }
+}
+
+// log("typeof Conf: %s", typeof Conf);
+// log("Conf: %s", JsonString(Conf));
+
+if( typeof Conf !== 'object' ) return error("Error: Conf is not Object.");
 
 var SMTP = Conf.SMTP;
 
@@ -40,3 +90,7 @@ transport.sendMail(message, function(err, success) {
     transport.close();
 });
 return;
+
+function JsonString(obj) {
+  return JSON.stringify(obj, null, "    ");
+}
